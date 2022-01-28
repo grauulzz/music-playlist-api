3e2c43667b859ca570168db693a3bf54b70bd1d3
Will Reynolds <billiamreynolds@gmail.com>
2022-01-27 19:31:35 -0600

    HandleRequest for all provider methods working

--- src/com/amazon/ata/music/playlist/service/lambda/GetPlaylistSongsActivityProvider.java
+++ src/com/amazon/ata/music/playlist/service/lambda/GetPlaylistSongsActivityProvider.java
@@ -1,14 +1,20 @@
 package com.amazon.ata.music.playlist.service.lambda;
 
 import com.amazon.ata.music.playlist.service.dependency.DaggerServiceComponent;
+import com.amazon.ata.music.playlist.service.dependency.ServiceComponent;
 import com.amazon.ata.music.playlist.service.models.requests.GetPlaylistSongsRequest;
 import com.amazon.ata.music.playlist.service.models.results.GetPlaylistSongsResult;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+
+// com.amazon.ata.music.playlist.service.lambda.GetPlaylistSongsActivityProvider::handleRequest
+
 public class GetPlaylistSongsActivityProvider implements RequestHandler<GetPlaylistSongsRequest,
         GetPlaylistSongsResult> {
+
+    ServiceComponent sc = DaggerServiceComponent.create();
 
     /**
      * Empty constructor for the lambda function.
@@ -24,9 +30,8 @@ public class GetPlaylistSongsActivityProvider implements RequestHandler<GetPlayl
     @Override
     public GetPlaylistSongsResult handleRequest(final GetPlaylistSongsRequest getPlaylistSongsRequest,
                                                 Context context) {
-        return DaggerServiceComponent.create()
-                .provideGetPlaylistSongsActivity()
-                .handleRequest(getPlaylistSongsRequest, context);
+        return sc.provideGetPlaylistSongsActivity().handleRequest(getPlaylistSongsRequest,
+                context);
     }
 
 }
--- src/com/amazon/ata/music/playlist/service/lambda/UpdatePlaylistActivityProvider.java
+++ src/com/amazon/ata/music/playlist/service/lambda/UpdatePlaylistActivityProvider.java
@@ -1,15 +1,16 @@
 package com.amazon.ata.music.playlist.service.lambda;
 
 import com.amazon.ata.music.playlist.service.dependency.DaggerServiceComponent;
+import com.amazon.ata.music.playlist.service.dependency.ServiceComponent;
 import com.amazon.ata.music.playlist.service.models.requests.UpdatePlaylistRequest;
 import com.amazon.ata.music.playlist.service.models.results.UpdatePlaylistResult;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-
+// com.amazon.ata.music.playlist.service.lambda.UpdatePlaylistActivityProvider::handleRequest
 public class UpdatePlaylistActivityProvider implements RequestHandler<UpdatePlaylistRequest, UpdatePlaylistResult> {
-
+    ServiceComponent sc = DaggerServiceComponent.create();
     /**
      * Empty constructor for the lambda function.
      */
@@ -23,10 +24,8 @@ public class UpdatePlaylistActivityProvider implements RequestHandler<UpdatePlay
      */
     @Override
     public UpdatePlaylistResult handleRequest(final UpdatePlaylistRequest updatePlaylistRequest, Context context) {
-        return DaggerServiceComponent.create()
-                .provideUpdatePlaylistActivity()
-                .handleRequest(updatePlaylistRequest, context);
+        return sc.provideUpdatePlaylistActivity().handleRequest(updatePlaylistRequest,
+                context);
     }
-
 }
 
